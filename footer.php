5abea169e911a5b44334b3791a5ebcabf0123c52
AsmareGirmay <girmayaron5@gmail.com>
2026-04-11 09:31:10 +0300

    updated footer and style design

--- footer.php
+++ footer.php
@@ -12,24 +12,15 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <div class="site-info">
+        <p>© 2026 MU-MIT Open Source Group Project</p>
+        <p>Built with WordPress | Developed by Filmon & Team</p>
+    </div>
+</footer>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<p>© MU-MIT Open Source Project</p>
 
 </body>
 </html>
